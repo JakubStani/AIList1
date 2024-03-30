e3626eda36c895e5d794c77d9a0da4d4235fe7d4
Jakub Staniszewski <kuba.staniszewski.ks@gmail.com>
2024-03-30 23:31:17 +0100

    develop dijkstraAlg

--- fixDijkstra.py
+++ fixDijkstra.py
@@ -134,14 +134,124 @@ def hourToSeconds(time):
 def dijkstraAlg(start, end, normalizedGraph, startTime):
 
     start['g']=0
+    start['arrivalTime']=secondsToHour(startTime)
+
+    #Lista nazw wierzchołków
+    qList=[]
+
     #ustawiamy g (czyli koszt najkrótszej ścieżki od startu do danego węzła) 
     #dla każdego węzła grafu, poza węzłem startowym
     #to mogę też chyba robić od razu, gdy tworzę noda tzn. ustawiać g na nieskończoność)
     for node in normalizedGraph:
         if not node==start['name']:
             normalizedGraph[node]['g']=float('inf')
-    
+            qList.append(node)
 
+    previousNode=start
+
+
+    while len(qList)>0:
+
+        #następnym wybranym węzłem będzie ten, o najmniejszym koszcie g
+        next_node=None
+        next_node_cost=float('inf')
+
+        chosenEdge=None
+
+        #TODO: tutaj też chcemy zapisać, do jakich węzłów jest już dostęp i przypisać im g (potem będziemy sprawdzać, czy istnieje szybszy dostęp do g)
+        #najpierw posortujmy wszystkie krawędzie
+        edgesSortedByDeparture=[] 
+        for edge in previousNode['edges']:
+            edgesSortedByDeparture=addToSortedEdgesList(edge, edgesSortedByDeparture) 
+
+        #musimy znaleźć wszystkie krawędzie, które mają wyjazd w przyszłości
+        edgesInTheFutureIndex=None
+        for i in range(len(edgesSortedByDeparture)):
+            if edgesSortedByDeparture[i]._departure_time()>=previousNode['arrivalTime']:
+                edgesInTheFutureIndex=i
+                break
+
+        #jeżeli są jakieś w przyszłości tego samego dnia...
+        if not edgesInTheFutureIndex==None:
+            #ponieważ edgesSortedByDeparture jest posortowane czasem wyjazdu, pozostałe tablice też będą
+            #szukamy linii z najszybszym przyjazdem (ale z przyjazdem w tym samym dniu lub z wyjazdem w tym samym dniu i przyjazdem w następnym)
+            chosenEdge=chooseEdgeWithFastestArrival(edgesSortedByDeparture[edgesInTheFutureIndex:len(edgesSortedByDeparture)], previousNode['arrivalTime'])
+
+        #jeżeli nie, wybieramy z tych następnego dnia (czyli z całej tablicy, bo wtedy wszystkie edge są następnego dnia)
+        else:
+            chosenEdge=chooseEdgeWithFastestArrival(edgesSortedByDeparture)
+
+
+
+        #PO WYBRANIU WĘZŁA DOCELOWEGO
+        
+        if(not chosenEdge==None):
+            chosenNode=normalizedGraph[chosenEdge._end_node()._stop_name()]
+            qList.remove[chosenNode['name']]
+            if(chosenNode['g']<previousNode['g']+(hourToSeconds(chosenEdge._arrival_time()) - hourToSeconds(previousNode['arrivalTime']))):
+                chosenNode['g']=previousNode['g']+(hourToSeconds(chosenEdge._arrival_time()) - hourToSeconds(previousNode['arrivalTime']))
+        
+
+        #na koniec trzeba ustawić previous noda na
+
+
+#listOfEdges nie może być pusta!!
+def chooseEdgeWithFastestArrival(listOfEdges, nodeFromTime=None):
+
+    fastestEdgeArrival='23:59:59'
+    fastestEdgeArrivalIndex=None
+
+    for i in range(len(listOfEdges)):
+        if listOfEdges[i]._arrival_time()<fastestEdgeArrival:
+            if(not nodeFromTime==None):
+                #chcemy uniknać sytuacji, że wybierze linię, która przyjeżdża o np. 00:00:01, a wyjeżdża o 23:59:59, jeżeli są lepsze opcje 
+                #(bo taka linia przyjeżdża o najszybszej godzinie, ale najszybszej w następnym dniu)
+                if listOfEdges[i]._arrival_time()>nodeFromTime:
+                    fastestEdgeArrival=listOfEdges[i]._arrival_time()
+                    fastestEdgeArrivalIndex=i
+            else:
+                #ta sytuacja ma miejsce, gdy przyjechaliśmy na przystanek i nie ma już żadnych linii tego dnia
+                #chcemy uniknać sytuacji, że wybierze linię, która najszybciej przyjeżdża, bo wyjeżdża poprzedniego dnia
+                if listOfEdges[i]._departure_time()>'00:00:00':
+                    fastestEdgeArrival=listOfEdges[i]._arrival_time()
+                    fastestEdgeArrivalIndex=i
+            
+    # if(fastestEdgeArrivalIndex==None):
+    #     list(map(lambda x: print(f'dep: {x._departure_time()}, arr: {x._arrival_time()}', listOfEdges)))
+                    
+    #jeżeli były tylko takie opcje, które wyjeżdżają jednego dnia, a dojeżdżają drugiego...
+    if(fastestEdgeArrivalIndex==None and not nodeFromTime==None):
+        #wybieramy tą z najszybszym dojazdem
+        fastestEdgeArrival='23:59:59'
+        for i in range(len(listOfEdges)):
+            if listOfEdges[i]._arrival_time()<fastestEdgeArrival:
+                fastestEdgeArrival=listOfEdges[i]._arrival_time()
+                fastestEdgeArrivalIndex=i
+
+    return listOfEdges[fastestEdgeArrivalIndex]
+
+#IMP: kod z gemini
+def secondsToHour(seconds):
+    return validateTime(time.strftime('%H:%M:%S', time.gmtime(seconds)))
+
+def addToSortedEdgesList(edge, edgesToCheck):
+    for i in range(len(edgesToCheck)):
+        if not len(edgesToCheck[i]._departure_time())==len(edge._departure_time()):
+            print('Niezgodne długości dat! 320')
+            print(f'{edgesToCheck[i]._departure_time()} vs {edge._departure_time()}, więc {edgesToCheck[i]._departure_time()>edge._departure_time()}')
+        if(edgesToCheck[i]._departure_time()>edge._departure_time()):
+            edgesToCheck.insert(i, edge)
+            return edgesToCheck
+    edgesToCheck.append(edge)
+    return edgesToCheck
+
+def validateTime(time):
+        time=time.split(':')
+        time[0]=int(time[0])
+        time[0]=str(time[0]%24)
+        if(len(time[0])==1):
+            time[0]=f'0{time[0]}'
+        return f'{time[0]}:{time[1]}:{time[2]}'
 
 if __name__=='__main__':
     normalizedGraph=buildGraphFromCSV('connection_graph.csv')
